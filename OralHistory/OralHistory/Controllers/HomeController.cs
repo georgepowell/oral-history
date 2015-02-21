5b9cc2f481924e1d3105bc4e259971262550c3e0
George Powell <georgepowell_1@hotmail.co.uk>
2015-02-21 17:10:51 +0000

    ASP.NET samlple app

---
+++ OralHistory/OralHistory/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OralHistory.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "RAAAA HEY I'M GEORGE!!";
+
+            return View();
+        }
+    }
+}
---
+++ OralHistory/OralHistory/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OralHistory.MvcApplication" Language="C#" %>
